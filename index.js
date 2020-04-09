5f80e31a37e4740869cef2f96280586587345b76
nicholas <bakoliasn@gmail.com>
2020-04-09 15:47:04 +0000

    dynamic ngrok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Smooch = require('smooch-core')
 const bodyParser = require('body-parser')
 const config = require('./config.js')
+const ngrok = require('ngrok')
 
 const app=express()
 const smooch = new Smooch({
@@ -23,7 +24,12 @@ app.get("/", function(req, res){
    process.env.port ||
    8000, function(){
    console.log(
-  'listening on: ' + process.env.port || ' localhost: 8000'
-  )
+  'listening on: ' + (process.env.port || ' localhost: 8000')  )
 })
+console.log("cd")
 
+async function expose(port){
+	const url = await ngrok.connect(port)
+	console.log(url)
+}
+expose(8000)
